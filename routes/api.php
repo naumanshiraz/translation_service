0854f9658ed24ab0f7a2e058bb06f2a7c75b052c
Nauman Shiraz <nauman_shiraz@hotmail.com>
2025-10-17 21:43:47 +0500

    Api routes created

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,12 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\LocaleController;
+use App\Http\Controllers\Api\TagController;
+use App\Http\Controllers\Api\TranslationController;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +20,41 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+Route::post('/login', function (Request $request) {
+  $request->validate([
+    'email' => ['required', 'email'],
+    'password' => ['required'],
+  ]);
+
+  $user = User::where('email', $request->email)->first();
+
+  if (! $user || ! Hash::check($request->password, $user->password)) {
+    throw ValidationException::withMessages([
+      'email' => ['The provided credentials are incorrect.'],
+    ]);
+  }
+
+  return response()->json([
+    'token' => $user->createToken('auth_token')->plainTextToken,
+    'message' => 'Logged in successfully!'
+  ]);
+});
+
+Route::middleware('auth:sanctum')->group(function () {
+  Route::get('/user', function (Request $request) {
     return $request->user();
+  });
+
+  Route::post('/logout', function (Request $request) {
+    $request->user()->currentAccessToken()->delete();
+    return response()->json(['message' => 'Logged out successfully!']);
+  });
+
+  Route::apiResource('locales', LocaleController::class);
+  Route::apiResource('tags', TagController::class);
+  Route::apiResource('translations', TranslationController::class);
+
+  Route::get('translations/search', [TranslationController::class, 'search']);
+
+  Route::get('translations/export/{localeCode}', [TranslationController::class, 'export']);
 });
