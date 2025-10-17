1d575b1b9cf5a69b94ef72e156fae226490f6491
Nauman Shiraz <nauman_shiraz@hotmail.com>
2025-10-17 21:32:15 +0500

    first commit

---
+++ app/Models/Translation.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Translation extends Model
+{
+  use HasFactory;
+
+  protected $fillable = ['locale_id', 'key', 'value'];
+
+  public function locale()
+  {
+    return $this->belongsTo(Locale::class);
+  }
+
+  public function tags()
+  {
+    return $this->belongsToMany(Tag::class);
+  }
+}
